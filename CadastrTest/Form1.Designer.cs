5cbbcd25bc570a1fbba1a9707d3b6c9a020e21e0
EvgeniQwerty <evgeniqwerty1337@gmail.com>
2021-06-07 13:46:40 +0300

    Добавьте файлы проекта.

---
+++ CadastrTest/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace CadastrTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openXMLToolStripMenuItem,
+            this.saveXMLToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openXMLToolStripMenuItem
+            // 
+            this.openXMLToolStripMenuItem.Name = "openXMLToolStripMenuItem";
+            this.openXMLToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openXMLToolStripMenuItem.Text = "Open XML...";
+            this.openXMLToolStripMenuItem.Click += new System.EventHandler(this.openXMLToolStripMenuItem_Click);
+            // 
+            // saveXMLToolStripMenuItem
+            // 
+            this.saveXMLToolStripMenuItem.Name = "saveXMLToolStripMenuItem";
+            this.saveXMLToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveXMLToolStripMenuItem.Text = "Save XML...";
+            this.saveXMLToolStripMenuItem.Click += new System.EventHandler(this.saveXMLToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(390, 426);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(390, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(410, 426);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Cadastr";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openXMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveXMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ CadastrTest/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CadastrTest
+{
+    public partial class Form1 : Form
+    {
+
+        private Dictionary<TreeNode, string> contentOfNodes = new Dictionary<TreeNode, string>(); //словарь для хранения содержимого нод
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             if (openFileDialog1.ShowDialog() == DialogResult.OK)
+             {
+                try
+                {
+                    //как только попадаем в головную ноду, начинаем запись стринга. Как только попадаем в его закрывающую часть, заканчиваем писать в стринг. Ещё делаем привязку к ноде (с помощью словаря)
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    XmlReaderSettings settings = new XmlReaderSettings();
+                    settings.Async = false; //без асинхронной загрузки/чтения
+                    XmlReader reader = XmlReader.Create(sr, settings);
+                    TreeNode addedNode, prevNode, landRecords, buildRecords, constructionRecords, spatialData, municipalBoundaries, zones;
+                    bool foundCadNumber = false; //переменная для идентификации тэга-идентификатора (cad_number, sk_id, reg_numb_border)
+                    string textXml = ""; //переменная, в которую записывается xml, для последующего показа в расшифровке (отправляется в словарь contentOfNodes)
+                    bool isSk_id = false; //переменная для идентификации, является ли тэг sk_id идентификатором или нет
+                    string prevTag = "land_record"; //переменная для сохранения предыдушего тэга
+                    bool isRightES = false; //переменная для идентификации, является ли тэг entity_spatial идентификатором или нет
+
+                    try
+                    {
+                        //считываем xml без пустых нод
+
+                        treeView1.Nodes.Clear(); //очистка перед загрузкой
+                        //инициализация переменных
+                        landRecords = treeView1.Nodes.Add("land_records");
+                        buildRecords = treeView1.Nodes.Add("build_records");
+                        constructionRecords = treeView1.Nodes.Add("construction_records");
+                        spatialData = treeView1.Nodes.Add("spatial_data");
+                        municipalBoundaries = treeView1.Nodes.Add("municipal_boundaries");
+                        zones = treeView1.Nodes.Add("zones_and_territories_boundaries");
+                        prevNode = landRecords;
+                        addedNode = landRecords;
+
+                        //цикл считывания
+                        while (reader.Read())
+                        {
+                            //свитч на тип ноды (открывающий тэг, текст, закрывающий тэг)
+                            switch (reader.NodeType)
+                            {
+                                case XmlNodeType.Element:
+                                    switch (reader.Name)
+                                    {
+                                        case "land_record":
+                                            prevNode = landRecords;
+                                            textXml = "<" + reader.Name + ">";
+                                            break;
+                                        case "cad_number":
+                                            if (prevTag != "common_land_cad_number")
+                                            {
+                                                foundCadNumber = true;
+                                            }
+                                            textXml += "<" + reader.Name + ">";
+                                            break;
+                                        case "build_record":
+                                            prevNode = buildRecords;
+                                            textXml = "<" + reader.Name + ">";
+                                            break;
+                                        case "construction_record":
+                                            prevNode = constructionRecords;
+                                            textXml = "<" + reader.Name + ">";
+                                            break;
+                                        case "entity_spatial":
+                                            if (prevTag != "contour" && prevTag != "number_pp")
+                                            {
+                                                prevNode = spatialData;
+                                                textXml = "<" + reader.Name + ">";
+                                                isRightES = true;
+                                            }
+                                            else
+                                            {
+                                                textXml += "<" + reader.Name + ">";
+                                                isRightES = false;
+                                            }
+                                            break;
+                                        case "sk_id":
+                                            isSk_id = true;
+                                            foundCadNumber = true;
+                                            textXml += "<" + reader.Name + ">";
+                                            break;
+                                        case "municipal_boundary_record":
+                                            prevNode = municipalBoundaries;
+                                            textXml = "<" + reader.Name + ">";
+                                            break;
+                                        case "reg_numb_border":
+                                            textXml += "<" + reader.Name + ">";
+                                            foundCadNumber = true;
+                                            break;
+                                        case "zones_and_territories_record":
+                                            prevNode = zones;
+                                            textXml = "<" + reader.Name + ">";
+                                            break;
+                                        default:
+                                            textXml += "<" + reader.Name + ">";
+                                            break;
+                                    }
+                                    prevTag = reader.Name;
+                                    break;
+                                case XmlNodeType.Text:
+                                    textXml += reader.Value;
+
+                                    if (isSk_id)
+                                    {
+                                        try
+                                        {
+                                            float value = float.Parse(reader.Value.Replace('.', ','));
+                                        }
+                                        catch
+                                        {
+                                            foundCadNumber = false;
+                                        }
+                                        finally
+                                        {
+                                            isSk_id = false;
+                                        }
+                                    }
+
+                                    if (foundCadNumber)
+                                    {
+                                        foundCadNumber = false;
+                                        addedNode = prevNode.Nodes.Add(reader.Value);
+                                    }
+                                    break;
+                                case XmlNodeType.EndElement:
+                                switch (reader.Name)
+                                {
+                                    case "land_record":
+                                        textXml += "</" + reader.Name + ">";
+                                        contentOfNodes.Add(addedNode, textXml);
+                                        break;
+                                    case "build_record":
+                                        textXml += "</" + reader.Name + ">";
+                                        contentOfNodes.Add(addedNode, textXml);
+                                        break;
+                                    case "construction_record":
+                                        textXml += "</" + reader.Name + ">";
+                                        contentOfNodes.Add(addedNode, textXml);
+                                        break;
+                                    case "entity_spatial":
+                                        textXml += "</" + reader.Name + ">";
+                                        if (isRightES)
+                                        { 
+                                            contentOfNodes.Add(addedNode, textXml);
+                                        }
+                                        isRightES = false;
+                                        break;
+                                    case "municipal_boundary_record": 
+                                        textXml += "</" + reader.Name + ">";
+                                        contentOfNodes.Add(addedNode, textXml);
+                                        break;
+                                    case "zones_and_territories_record": 
+                                        textXml += "</" + reader.Name + ">";
+                                        contentOfNodes.Add(addedNode, textXml);
+                                        break;
+                                    default:
+                                        textXml += "</" + reader.Name + ">";
+                                        break;
+                                }
+                                break;
+
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка при загрузке файла!");
+                    }
+                    finally
+                    {
+                        //закрываем считывание
+                        if (reader != null)
+                            reader.Close();
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Ошибка доступа.\n\nСообщение об ошибке: {ex.Message}\n\n" +
+                    $"Детали:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        //функция для красивого вывода xml с отступами
+        private string FormatXml(string xml)
+        {
+            try
+            {
+                XDocument doc = XDocument.Parse(xml);
+                return doc.ToString();
+            }
+            catch
+            {
+                return xml;
+            }
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                //выбираем ноду с фокусом. Ищем её в словаре. Если находим, в текстбокс выводим данные.
+                TreeView selectedNode = (TreeView)sender;
+                string result = contentOfNodes[selectedNode.SelectedNode];
+                richTextBox1.Text = FormatXml(result);
+            }
+            catch
+            {
+                richTextBox1.Text = "";
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+
+                saveFileDialog1.CreatePrompt = true;
+                saveFileDialog1.OverwritePrompt = true;
+                saveFileDialog1.FileName = "cadastr";
+                saveFileDialog1.DefaultExt = "xml";
+                saveFileDialog1.Filter =
+                    "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+                saveFileDialog1.InitialDirectory =
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                DialogResult result = saveFileDialog1.ShowDialog();
+
+                if (saveFileDialog1.FileName.Length == 0)
+                {
+                    MessageBox.Show("Заполните имя файла!");
+                }
+
+                else if (result == DialogResult.OK)
+                {
+                    string[] arr_text = richTextBox1.Lines;
+                    //richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                    File.WriteAllLines(saveFileDialog1.FileName, arr_text, Encoding.GetEncoding("utf-8"));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не выбрана нода для сохранения!");
+            }
+        }
+    }
+}
